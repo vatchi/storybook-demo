2e3bc8fe5b44999fb9f4f2e9faa387a6aae8a74b
Love Hermansson <love.hermansson@visma.com>
2021-02-08 15:39:06 +0100

    fix: testing new export

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue';
+// import Vue from 'vue';
 
 import CtaButton from './components/CtaButton/CtaButton.vue';
 import SearchBar from './components/SearchBar/SearchBar.vue';
 
-const Components = {
-    CtaButton,
-    SearchBar
-};
+// const Components = {
+//     CtaButton,
+//     SearchBar
+// };
 
-Object.keys(Components).forEach( name => {
-    Vue.component(name, Components[name]);
-});
+// Object.keys(Components).forEach( name => {
+//     Vue.component(name, Components[name]);
+// });
 
-export default Components;
+// export default Components;
+
+export default { CtaButton, SearchBar };
